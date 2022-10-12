cb6eee9f5d257c3eca59771825f5b8e8e5d22baa
Francisco Javier Sampedro Nuñez <franjsampedro@gmail.com>
2022-10-12 19:19:10 +0200

    Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,11 +5,33 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all,setAll]=useState(0)
+  const [score, setScore]=useState(0)
 
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
-  
+
+  const handleGood = () => {
+    let gud=good+1
+    setGood(gud)
+    setAll(getAll(gud,bad,neutral))
+    setScore(score+1)
+  }
+  const handleNeutral = () => {
+    let neutr=neutral+1
+    setNeutral(neutr)
+    setAll(getAll(good,bad,neutr))
+
+  }
+  const handleBad = () => {
+    let bd = bad+1
+    setBad(bd)
+    setAll(getAll(good,bd,neutral))
+    setScore(score-1)
+  }
+  const getAll = (good, bad, neutral) =>
+  {
+   return (good+bad+neutral) 
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -22,6 +44,12 @@ const App = () => {
       <a>neutral: {neutral}</a>
       <br></br>
       <a>bad: {bad}</a>
+      <br></br>
+      <a>all: {all}</a>
+      <br></br>
+      <a>average: {score/all}</a>
+      <br></br>
+      <a>percent: {good/all}%</a>
     </div>
   )
 }
