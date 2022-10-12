f53caedc91f0ac44dc7998c5d17606b9e0832d29
Francisco Javier Sampedro Nuñez <franjsampedro@gmail.com>
2022-10-12 18:44:42 +0200

    more samples

---
+++ part1/samples/complex-state/src/App.js
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0, allClicks: []
+  })
+
+  const hello = (who) => {
+    const handler = () => {
+      console.log('hello', who)
+    }
+    return handler
+  }
+  //Esto es lo mismo que:
+  //const hello = (who) => () => {
+  //console.log('hello', who)
+  //}
+  //Una arrow function que devuelve una arrow function definida en su interior
+  
+  const handleLeftClick = () => {
+    setClicks({ ...clicks, left: clicks.left + 1, allClicks: clicks.allClicks.concat("L") })
+  }
+  //No se debe incrementar el estado directamente en react, siempre hay que crear un objeto nuevo. 
+  //No se debe usar por ejemplo click.left++ o click.allClicks.push()
+
+  const handleRightClick = () => {
+    setClicks({ ...clicks, right: clicks.right + 1, allClicks: clicks.allClicks.concat("R") })
+  }
+
+  return (
+    <div>
+      <button onClick={hello("almendrico")}>di hola al almendrico en la consola</button>
+      <br></br>
+      {clicks.left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {clicks.right}
+      <br></br>
+      <History allClicks={clicks.allClicks} />
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+export default App;
