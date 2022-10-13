3769c4e3e67e6cdaa5ac36060b5ea4b9c82b7a16
Francisco Javier Sampedro Nuñez <franjsampedro@gmail.com>
2022-10-13 10:23:49 +0200

    Exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,32 +5,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all,setAll]=useState(0)
-  const [score, setScore]=useState(0)
+  const [all, setAll] = useState(0)
+  const [score, setScore] = useState(0)
 
   const handleGood = () => {
-    setGood(good+1)
-    setAll(all+1)
-    setScore(score+1)
+    setGood(good + 1)
+    setAll(all + 1)
+    setScore(score + 1)
   }
   const handleNeutral = () => {
-    setNeutral(neutral+1)
-    setAll(all+1)
+    setNeutral(neutral + 1)
+    setAll(all + 1)
   }
   const handleBad = () => {
-    setBad(bad+1)
-    setAll(all+1)
-    setScore(score-1)
+    setBad(bad + 1)
+    setAll(all + 1)
+    setScore(score - 1)
   }
 
   return (
-    <div>
-      <h1>give feedback</h1>
-      <Button handleClick = {handleGood} text="good"/>
-      <Button handleClick = {handleNeutral} text="neutral"/>
-      <Button handleClick = {handleBad} text="bad"/>
+    <>
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={handleGood} text="good" />
+        <Button handleClick={handleNeutral} text="neutral" />
+        <Button handleClick={handleBad} text="bad" />
+      </div>
       <Statistics good={good} bad={bad} neutral={neutral} score={score} all={all} />
-    </div>
+    </>
   )
 }
 
@@ -39,38 +41,42 @@ const Button = ({ handleClick, text }) => (
     {text}
   </button>
 )
-const StatisticsLine = ({text,value,unit})=>(
-  <>
-  <a>{text} : {value} {unit}</a>
-  <br></br>
-  </>
+const StatisticsLine = ({ text, value, unit }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value} {unit}</td>
+  </tr>
 )
 
-const Statistics= (props) =>{
-  let good=props.good
-  let bad=props.bad
-  let neutral=props.neutral
-  let score=props.score
-  let all=props.all
-  if (all>0) { 
-    return(
+const Statistics = (props) => {
+  let good = props.good
+  let bad = props.bad
+  let neutral = props.neutral
+  let score = props.score
+  let all = props.all
+  if (all > 0) {
+    return (
       <>
-      <h2>statistics</h2>
-      <StatisticsLine text="good" value = {good}></StatisticsLine>
-      <StatisticsLine text="neutral" value = {neutral}></StatisticsLine>
-      <StatisticsLine text="bad" value = {bad}></StatisticsLine>
-      <StatisticsLine text="all" value = {all}></StatisticsLine>
-      <StatisticsLine text="average" value = {score/all}></StatisticsLine>
-      <StatisticsLine text="percent" value = {good/all} unit="%"></StatisticsLine>
+        <h2>statistics</h2>
+        <table>
+          <tbody>
+            <StatisticsLine text="good" value={good}></StatisticsLine>
+            <StatisticsLine text="neutral" value={neutral}></StatisticsLine>
+            <StatisticsLine text="bad" value={bad}></StatisticsLine>
+            <StatisticsLine text="all" value={all}></StatisticsLine>
+            <StatisticsLine text="average" value={score / all}></StatisticsLine>
+            <StatisticsLine text="percent" value={good / all} unit="%"></StatisticsLine>
+          </tbody>
+        </table>
       </>
     )
-    }
-  else{
-    return(
+  }
+  else {
+    return (
       <>
-      <h2>statistics</h2>
-      <a>No feedback given</a>
-      <br></br>
+        <h2>statistics</h2>
+        <a>No feedback given</a>
+        <br></br>
       </>
     )
   }
