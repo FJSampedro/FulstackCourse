78a7f8076b240d740c91a204e3f80b65937223fe
Francisco Javier Sampedro Nuñez <franjsampedro@gmail.com>
2022-11-03 08:18:31 +0100

    Exercise 2.

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,10 +1,12 @@
 import Part from "./Part"
+import Total from "./Total"
 const Content = (props) => {
     return (
       <>
         <Part part={props.parts[0]}></Part>
         <Part part={props.parts[1]}></Part>
         <Part part={props.parts[2]}></Part>
+        <Total parts={props.parts}></Total>
       </>
     )
   }
