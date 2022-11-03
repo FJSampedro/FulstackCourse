64b5ad9a2cb68af0e2d4fdca5667cc4208e338c8
Francisco Javier Sampedro Nuñez <franjsampedro@gmail.com>
2022-11-03 08:40:10 +0100

    Exercise 2.4 & 2.5

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -2,10 +2,7 @@ import Part from "./Part"
 import Total from "./Total"
 const Content = (props) => {
     return (
-      <>
-        <Part part={props.parts[0]}></Part>
-        <Part part={props.parts[1]}></Part>
-        <Part part={props.parts[2]}></Part>
+      <>{props.parts.map(part=><Part key={part.id} part={part}/>)}
         <Total parts={props.parts}></Total>
       </>
     )
