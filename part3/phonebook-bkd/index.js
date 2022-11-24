a722553fad38545bf440ab48da22750f5704ca3f
Francisco Javier Sampedro Nuñez <franjsampedro@gmail.com>
2022-11-24 07:59:28 +0100

    Exercise 3.3

--- part3/phonebook-bkd/index.js
+++ part3/phonebook-bkd/index.js
@@ -35,6 +35,16 @@ app.get('/info', (request, response) => {
     response.render('index', { title: 'Phonebook info', personsNumber: persons.length, date:Date().toLocaleString()})
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(note => note.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`);
