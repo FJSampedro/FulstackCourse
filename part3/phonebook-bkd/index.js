bf29356c9457d0471cd5fca988d66858e3e05a79
Francisco Javier Sampedro Nuñez <franjsampedro@gmail.com>
2022-12-08 16:03:12 +0100

    Exercise 3.13

--- part3/phonebook-bkd/index.js
+++ part3/phonebook-bkd/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config(".env") 
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
+const Person = require('./models/person')
 
 const app = express()
 morgan.token("exercise",function (req,res){return JSON.stringify(req.body)})
@@ -11,50 +13,25 @@ app.use(morgan(":method :url :status :res[content-length] - :response-time ms :e
 
 app.set('view engine', 'pug')
 
-let persons = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2
-    },
-    {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3
-    },
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
-    }
-]
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
-
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
 
 app.get('/info', (request, response) => {
     console.log();
     response.render('index', { title: 'Phonebook info', personsNumber: persons.length, date: Date().toLocaleString() })
 })
 
+app.get('/api/persons', (request, response) => {
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
+})
+
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(note => note.id === id)
-    if (person) {
+    Person.findById(request.params.id).then(person => {
         response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    }).catch(error=>{response.status(404).end()})
 })
 
 app.delete('/api/persons/:id', (request, response) => {
