b20cfc7e7eacca7c7ddaf3b3485830bb3d044eb0
Francisco Javier Sampedro Nuñez <franjsampedro@gmail.com>
2022-12-08 16:36:37 +0100

    Samples: Catch errors.

--- part3/samples/express-server/index.js
+++ part3/samples/express-server/index.js
@@ -32,10 +32,20 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
-    })
-})
+    Note.findById(request.params.id)
+      .then(note => {
+        if (note) {
+          response.json(note)
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error => {
+        console.log(error)
+        response.status(500).end()
+        //response.status(400).send({ error: 'malformatted id' })
+      })
+  })
 
 app.delete('/api/notes/:id', (request, response) => {
     Note.findByIdAndDelete(request.params.id).then(response.status(204).end())
