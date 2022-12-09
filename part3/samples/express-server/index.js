6bd231724a8f597fd6da29409d3900415b9e2c45
Francisco Javier Sampedro Nuñez <franjsampedro@gmail.com>
2022-12-09 13:27:53 +0100

    Samples: Catch errors by middleware

--- part3/samples/express-server/index.js
+++ part3/samples/express-server/index.js
@@ -7,16 +7,24 @@ const app = express()
 app.use(cors())
 app.use(express.json()) //esto es una llamada a un middleware, una funcion que express ejecuta sobre la peticion antes de darla a nuestra funcion
 
+const errorHandler = (error, request, response, next) => { //Middleware manejador de errores
+    console.error(error.message)
+
+    if (error.name === 'CastError') { //comprueba si es un cast error, lo que implica ID de objeto no válido para Mongo
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler) //Llamada al middleware de gestion de errores
+
 const requestLogger = (request, response, next) => { // Esta es una definicion de un custom middleware, siempre tienen que tener estos tres parametros.
     console.log('Method:', request.method) //los middleware suelen definirse y llamarse ANTES de las rutas 
     console.log('Path:  ', request.path)  //pero tambien pueden ser llamados despues de ellas para ser usados en caso de que las rutas anteriores no gestionen la peticion
     console.log('Body:  ', request.body)
     console.log('---')
     next()
-}
-
-const unknownEndpoint = (request, response) => { //Custom middleware que gestiona un endpoint desconocido
-    response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(requestLogger) //llamada a nuestro custom middleware
@@ -31,24 +39,42 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id)
-      .then(note => {
-        if (note) {
-          response.json(note)
-        } else {
-          response.status(404).end()
-        }
-      })
-      .catch(error => {
-        console.log(error)
-        response.status(500).end()
-        //response.status(400).send({ error: 'malformatted id' })
-      })
-  })
+// Manejo de errores directamente en el codigo de respuesta
+// app.get('/api/notes/:id', (request, response) => {
+//     Note.findById(request.params.id)
+//         .then(note => {
+//             if (note) {
+//                 response.json(note)
+//             } else {
+//                 response.status(404).end()
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error)
+//             response.status(500).end()
+//             //response.status(400).send({ error: 'malformatted id' })
+//         })
+// })
 
-app.delete('/api/notes/:id', (request, response) => {
-    Note.findByIdAndDelete(request.params.id).then(response.status(204).end())
+// Manejo de errores mediante middleware
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -71,6 +97,24 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => { //Custom middleware que gestiona un endpoint desconocido
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 app.use(unknownEndpoint) //llamada al middleware que gestiona un endpoint desconocido
 
 const PORT = process.env.PORT || 3001
