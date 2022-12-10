62b9019e501055496fe91cfa13dce7e164c1d5f4
Francisco Javier Sampedro Nuñez <franjsampedro@gmail.com>
2022-12-10 21:05:55 +0100

    Exercise 3.19

--- part3/phonebook-bkd/index.js
+++ part3/phonebook-bkd/index.js
@@ -37,7 +37,7 @@ app.delete('/api/persons/:id', (request, response) => {
     .catch(error=>next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name) {
@@ -56,9 +56,11 @@ app.post('/api/persons', (request, response) => {
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
+    person.save()
+    .then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error=> next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -80,8 +82,10 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError') { 
         return response.status(400).send({ error: 'wrong id' })
     }
-    else
-    {
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+      }
+    else    {
         return response.status(500).end()
     }
 }
--- part3/phonebook-bkd/models/person.js
+++ part3/phonebook-bkd/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')//loads the mongoose-unique-validator
 mongoose.set('strictQuery', true);
 
 const url = process.env.MONGODB_URI
@@ -14,9 +15,10 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({    
-    name: String,
+    name: { type: String, required: true, unique: true },
     number: String
 })
+personSchema.plugin(uniqueValidator) //Loads the uniqueValidator into the schema
 
 personSchema.set('toJSON', {                  
     transform: (document, returnedObject) => {
